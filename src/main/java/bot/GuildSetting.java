15f05083e838bbc52817891a82359e55aa1efc06
William Li <williamhli0707@gmail.com>
2022-06-03 12:03:57 -0700

    test

--- src/main/java/bot/GuildSetting.java
+++ src/main/java/bot/GuildSetting.java
@@ -3,7 +3,21 @@ package bot;
 public class GuildSetting {
     String serverLang;
     boolean xSaid, allChannels;
+    long channel;
 	public GuildSetting(){
 
     }
+    public String getServerLang() {
+        return serverLang;
+    }
+    public boolean isxSaid() {
+        return xSaid;
+    }
+    public boolean isAllChannels() {
+        return allChannels;
+    }
+    public long getChannel() {
+        return channel;
+    }
+
 }
